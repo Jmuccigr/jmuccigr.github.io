55abe45e6dedce279a3acb607d643618cd5ad429
John Muccigrosso <john@muccigrosso.org>
2016-08-18 12:07:43 -0400

    Deleted failed attempt

--- sub/cv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/john_muccigrosso/Documents/github/local/stuff
-pandoc -s --filter pandoc-fignos --atx-headers --css http://users.drew.edu/jmuccigr/css/clean_md.css ./CV.markdown -o CV.html
-pandoc -s --filter pandoc-fignos --atx-headers --css http://users.drew.edu/jmuccigr/css/clean_md_print.css ./CV.markdown -o CV_pdf.html
