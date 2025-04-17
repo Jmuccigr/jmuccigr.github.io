e735cf24f19cdba1997cdaa6c7686704a430cc88
John Muccigrosso <john@muccigrosso.org>
2025-04-17 12:03:15 +0200

    pandoc command update

--- CV/cv.sh
+++ CV/cv.sh
@@ -4,4 +4,4 @@ me=`whoami`
 thisdir=`dirname "$0"`
 cd $thisdir
 
-pandoc --self-contained --template="/Users/$me/Documents/github/local/pandoc-templates/default.html4"  -s --columns 800 --bibliography="/Users/$me/Documents/github/local/miscellaneous/My Library.json" ./CV.markdown -f markdown+smart -o CV.html
+pandoc --embed-resources --standalone --template="/Users/$me/Documents/github/local/pandoc-templates/default.html4" -s --columns 800 --bibliography="/Users/$me/Documents/github/local/miscellaneous/My Library.json" ./CV.markdown -f markdown+smart -o CV.html
